de5feceaf4531b5de1b562ec12cd006d74c08de4
Jannis Konrad <kabel42@gmail.com>
2025-08-02 01:22:50 +0200

    read sensor

--- py/main.py
+++ py/main.py
@@ -1,6 +1,9 @@
 import time
 import usb.device
 from usb.device.scroll import ScrollInterface
+from machine import Pin, I2C
+import struct
+
 s = ScrollInterface()
 usb.device.get().init(s, builtin_driver=True)
 
@@ -9,9 +12,27 @@ while not s.is_open():
 
 time.sleep_ms(100)
 
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400_000)
+
+last_angle = struct.unpack(">H", i2c.readfrom_mem(0x36, 0x0c, 2))[0]
+
+print("Go", last_angle)
+
 while True:
-    if rp2.bootsel_button():
-        print("scroll")
-        s.send_report(1)
-        while rp2.bootsel_button():
-            time.sleep_ms(100)
+    agc = struct.unpack("B", i2c.readfrom_mem(0x36, 0x1a, 1))[0]
+    if agc > 100:
+        print("no magnet")
+        time.sleep_ms(100)
+    else:
+        angle = struct.unpack(">H", i2c.readfrom_mem(0x36, 0x0c, 2))[0]
+        diff = angle-last_angle
+        if diff > 2048:
+            diff -= 4096
+        elif diff < -2048:
+            diff += 4096
+        if diff:
+            print(angle, last_angle, diff)
+        last_angle = angle
+    
+#       if diff < 128 and diff > -128:
+#           s.send_report(diff)
