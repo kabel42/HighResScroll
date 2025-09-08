d9609f527871835d793cbf353c001baf56fb0795
Jannis Konrad <kabel42@gmail.com>
2025-09-08 22:57:14 +0200

    use LED to indicate presence of magnet

--- py/AS5600_main.py
+++ py/AS5600_main.py
@@ -11,6 +11,10 @@ usb.device.get().init(s, builtin_driver=True)
 while not s.is_open():
     time.sleep_ms(100)
 
+n = neopixel.NeoPixel(Pin(16), 1)
+n[0] = (0, 0, 255)
+n.write()
+
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400_000)
 
 def readAngle():
@@ -20,12 +24,22 @@ def getMagnetPresent():
 
 last_angle = readAngle()
 print("Go", last_angle)
+magOk = False
 
 while True:
     if not getMagnetPresent():
-        print("no magnet")
+        if magOk:
+            print("no magnet")
+            n[0] = (255, 0, 0)
+            n.write()
+            magOk = False
         time.sleep_ms(100)
     else:
+        if not magOk:
+            n[0] = (0, 255, 0)
+            n.write()
+            magOk = True
+
         angle = readAngle()
         diff = angle-last_angle
         if diff > 2048:
@@ -37,3 +51,4 @@ while True:
             if diff < 128 and diff > -128:
                 s.send_report(diff)
             last_angle = angle
+        time.sleep_ms(10)
